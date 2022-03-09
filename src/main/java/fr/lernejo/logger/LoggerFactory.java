3a72af2df9dcb1f9154abeacf703deb04ce2eb89
nmolina42 <58724725+nicolaas91@users.noreply.github.com>
2022-03-09 15:09:19 +0100

    first commit

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,16 @@
+package fr.lernejo.logger;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LoggerFactory {
+
+    public static @NotNull Logger getLogger(String name) {
+        return new ContextualLogger(name, new CompositeLogger(
+                new ConsoleLogger(),
+                new FilteredLogger(
+                        new FileLogger(System.getProperty("user.home") + "/Desktop/file.txt"),
+                        s -> s.contains("Simulation")
+                )
+        ));
+    }
+}
